c7715e37b2a6bc7473871d0a287d65a6615faa23
dandriyal8449-coder <dandriyal8449@gmail.com>
2026-01-14 15:23:56 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Home from './pages/Home'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
+import Hero from './components/Hero/Hero'
+import Companies from './components/Companies/Companies'
+import Services from './components/Services/Services'
+import Work from './components/Work/Work'
+import Team from './components/Team/Team'
+import Contact from './components/Contact/Contact'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <Hero />
+      <Companies />
+      <Services />
+      <Work />
+      <Team />
+      <Contact />
       <Footer />
     </>
   )
